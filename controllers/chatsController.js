25e73c991e71744d8aa7b371941bad4942867d34
MiheerSharma22 <miheer.sharma1@gmail.com>
2024-08-06 23:08:15 +0530

    saving the message received using socket into DB and also pushing its entry into user's chats array at top (0th index)

--- controllers/chatsController.js
+++ controllers/chatsController.js
@@ -1,13 +1,29 @@
 const UserChat = require("../model/chat"); // Mongoose model for Message
+const UserData = require("../model/userInfo");
 
 const createSocketConnection = (socket, io) => {
+  // todo: this runs once, when new connection is created, same for disconnect, but socket events handlers run everytime, how -> read
   console.log("New client connected");
 
+  // todo: is name "sendMessage" predefined or we can give any custom name to event
   socket.on("sendMessage", async (message) => {
     try {
-      const newMessage = new UserChat(message);
-      await newMessage.save();
-      io.emit("message", newMessage); // Emit the message to all clients
+      const newMessage = await UserChat.create({ chatMessage: message });
+
+      // pushing this message into user's array of chats
+      const updatedChats = await UserData.findOneAndUpdate(
+        {
+          email: "miheer.sharma1@gmail.com",
+        },
+        {
+          $push: { chats: { $each: [newMessage._id], $position: 0 } },
+        },
+        { new: true }
+      );
+      console.log("updatedChats: ", updatedChats);
+
+      // todo: UNDERSTAND what emit does, Emit the message to all clients
+      io.emit("message", newMessage);
     } catch (err) {
       console.error("Error saving message:", err);
     }
