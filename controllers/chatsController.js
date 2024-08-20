a9e3563a685b320514cac6c19b926c0ead2980c0
MiheerSharma22 <miheer.sharma1@gmail.com>
2024-08-20 22:48:11 +0530

    generating the AI response in the socket handler, after the user message is received and storing it in user's chats collection

--- controllers/chatsController.js
+++ controllers/chatsController.js
@@ -1,25 +1,49 @@
 const UserChat = require("../model/chat"); // Mongoose model for Message
 const UserData = require("../model/userInfo");
+const { generateResponse } = require("./generateChatResponse");
 
 const createSocketConnection = (socket, io) => {
   console.log("New client connected");
 
   socket.on("sendMessage", async (message) => {
     try {
-      const newMessage = await UserChat.create({ chatMessage: message });
+      // create a new entry in user-chats collections
+      const newUserMessage = await UserChat.create({ chatMessage: message });
 
+      // todo: try to push both user message and the AI message into dB at once
       // pushing this message into user's array of chats
       const updatedChats = await UserData.findOneAndUpdate(
         {
+          // todo: fetch this email dynamically
           email: "miheer.sharma1@gmail.com",
         },
         {
-          $push: { chats: { $each: [newMessage._id], $position: 0 } },
+          $push: { chats: { $each: [newUserMessage._id], $position: 0 } },
         },
         { new: true }
       );
 
-      io.emit("message", newMessage);
+      // generate chat response
+      const aiResponse = await generateResponse(message);
+
+      // store AI response chat message in user-chat collections in DB
+      const newResponseMessage = await UserChat.create({
+        chatMessage: aiResponse,
+        isAIResponse: true,
+      });
+
+      // pushing this response message into user's array of chats
+      const updatedChatsAfterResponse = await UserData.findOneAndUpdate(
+        {
+          email: "miheer.sharma1@gmail.com",
+        },
+        {
+          $push: { chats: { $each: [newResponseMessage._id], $position: 0 } },
+        },
+        { new: true }
+      );
+
+      socket.emit("response", aiResponse);
     } catch (err) {
       console.error("Error saving message:", err);
     }
