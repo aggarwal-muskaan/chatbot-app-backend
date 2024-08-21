8e815e1a286e1adb9d8bf48cabbca870ca3e9ba3
MiheerSharma22 <miheer.sharma1@gmail.com>
2024-08-21 22:39:52 +0530

    created a function to set the chat context for user ad model part

--- controllers/generateChatResponse.js
+++ controllers/generateChatResponse.js
@@ -1,31 +1,51 @@
-const { GoogleGenerativeAI } = require("@google/generative-ai");
+const {
+  GoogleGenerativeAI,
+  GenerativeModel,
+  ChatSession,
+  StartChatParams,
+} = require("@google/generative-ai");
+const UserChat = require("../model/chat"); // Mongoose model for Message
+
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-const chat = model.startChat({
-  history: [
-    // TODO: handle contexts
-    // user context
-    {
-      role: "user",
-      parts: [
-        {
-          text: "",
-        },
-      ],
-    },
-    // AI (model) context
-    {
-      role: "model",
-      parts: [
-        {
-          text: "",
-        },
-      ],
-    },
-  ],
-});
+let chat;
+
+exports.setChatContext = async () => {
+  console.log("Insdie Set Context ()");
+  // need to set the context when socket connection is established
+  const lastSixChatMessages = await UserChat.find()
+    .sort({ _id: -1 }) // Sort by _id in descending order to get the latest entries
+    .limit(6) // Limit the results to the latest 6 entries
+    .exec();
+
+  const userChatMessages = lastSixChatMessages.filter(
+    (message) => !message.isAIResponse
+  );
+  const modelResponse = lastSixChatMessages.filter(
+    (message) => message.isAIResponse
+  );
+
+  chat = model.startChat({
+    history: [
+      // user context
+      {
+        role: "user",
+        parts: userChatMessages.map((userMessage) => ({
+          text: userMessage.chatMessage,
+        })),
+      },
+      // AI (model) context
+      {
+        role: "model",
+        parts: modelResponse.map((response) => ({
+          text: response.chatMessage,
+        })),
+      },
+    ],
+  });
+};
 
 // handler to generate response
 exports.generateResponse = async (userMessage) => {
