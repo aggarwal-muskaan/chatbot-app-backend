c1f3bd2a50b8d5a8b67e0769eeedbed15b790915
MiheerSharma22 <miheer.sharma1@gmail.com>
2024-09-01 13:12:06 +0530

    added routes to save a chat message and to retrieve all aved chat messages list

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,8 @@
 // importing the controllers
 const { login } = require("../controllers/authController");
 const { getAllMessages } = require("../controllers/getAllMessages");
+const { saveChatMessage } = require("../controllers/saveChatController");
+const { getAllSavedMessages } = require("../controllers/getAllSavedMessages");
 const { isTokenValid } = require("../middleware/auth");
 
 const express = require("express");
@@ -9,6 +11,13 @@ const router = express.Router();
 // creating routes and attaching their associated controllers
 router.post("/auth/google", login);
 
+// save a message
+router.put("/saveChatMessage", isTokenValid, saveChatMessage);
+
+// get all chats
 router.get("/getAllMessages", isTokenValid, getAllMessages);
+
+// get saved chat messages
+router.get("/getAllSavedMessages", isTokenValid, getAllSavedMessages);
 
 module.exports = router;
