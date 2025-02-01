a77facfcc151b8fb3790e61fb648f2a1bd1a58e2
MiheerSharma22 <miheer.sharma1@gmail.com>
2025-02-01 19:29:21 +0530

    handler function to get user's itineries chat messages

---
+++ controllers/getAllItineraries.js
@@ -0,0 +1,32 @@
+const Itineraries = require("../model//itinerary");
+const UserData = require("../model/userInfo");
+
+exports.getAllItineraries = async (request, response) => {
+  try {
+    const { email } = request.user;
+    const user = await UserData.findOne({ email });
+
+    if (!user)
+      return response.status(401).json({
+        success: false,
+        message: "Unauthorised user",
+      });
+
+    const allUserItineraries = await Itineraries.find({
+      _id: { $in: user.itnieraries },
+    }).populate();
+
+    return response.status(200).json({
+      success: true,
+      data: {
+        allUserItineraries,
+      },
+    });
+  } catch (error) {
+    console.log("Error while fetching the Itineraries: ", error);
+    return response.send(500).json({
+      success: false,
+      message: `error in fethcing Itineraries: ${error}`,
+    });
+  }
+};
