fea41cb285b7312fb0f10363f127151dd4fa4585
MiheerSharma22 <miheer.sharma1@gmail.com>
2025-04-27 20:17:59 +0530

    passing chatType to setContext handler function to determin the type of chat and deal with the respective chat model for setting the message context/history.

--- controllers/generateChatResponse.js
+++ controllers/generateChatResponse.js
@@ -1,5 +1,7 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const UserChat = require("../model/chat"); // Mongoose model for Message
+const Recipes = require("../model/recipe");
+const Itineraries = require("../model/itinerary");
 const UserData = require("../model/userInfo");
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
@@ -7,15 +9,35 @@ const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 // handler to generate response
-exports.setContextGenerateResponse = async ({ userEmail, userNewMessage }) => {
+exports.setContextGenerateResponse = async ({
+  userEmail,
+  userNewMessage,
+  chatType,
+}) => {
   const user = await UserData.find({ email: userEmail });
 
-  const chatIds = user[0]?.chats?.slice(0, 6);
+  // finding the last 6 chatIds of user based on the type of chat the user is on frontend.
+  const chatIds =
+    chatType === "chat"
+      ? user[0]?.chats?.slice(0, 6)
+      : chatType === "recipe"
+      ? user[0]?.recipes?.slice(0, 6)
+      : user[0]?.itineraries?.slice(0, 6);
 
   // need to set the context when socket connection is established
-  const lastSixChatMessages = await UserChat.find({
-    _id: { $in: chatIds },
-  }).populate();
+  // based on the type of chats, finding chats in associated chat models
+  const lastSixChatMessages =
+    chatType === "chat"
+      ? await UserChat.find({
+          _id: { $in: chatIds },
+        }).populate()
+      : chatType === "recipe"
+      ? await Recipes.find({
+          _id: { $in: chatIds },
+        }).populate()
+      : await Itineraries.find({
+          _id: { $in: chatIds },
+        }).populate();
 
   // getting the last 3 user messages
   const userChatMessages = lastSixChatMessages.filter(
